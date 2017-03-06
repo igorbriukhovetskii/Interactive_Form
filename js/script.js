e3a2abffa9d603a994d9986a213652ecb80c65f6
igorbriukhovetskii <igorbruhoveckij@gmail.com>
2017-03-06 15:01:07 +0300

    Completed activities section

--- js/script.js
+++ js/script.js
@@ -47,16 +47,6 @@
     /**--------------------------------------------------------------------------------------------------*/
 
 
-
-
-
-
-
-
-
-
-
-
     /**---------------------------------------------------------------------------------------------------*/
 
     /** CSS class for hide an HTML element */
@@ -95,17 +85,15 @@
           activitiesPrices[i] = activity.parentNode.innerText;
           activitiesPrices[i] = activitiesPrices[i].match(/\d{2,5}$/);
           activitiesPrices[i] = parseInt(activitiesPrices[i], 10);
-      })
+      });
     };
 
     /** Function counts a running total for activities */
     var countActivitiesTotalPrice = function () {
         var price = 0;
-        totalPrice = 0;
-
+        
         [].forEach.call(activities, function (activity, i) {
            if (activity.checked === true) {
-               console.log(activitiesPrices[i]);
                price += activitiesPrices[i];
            }
         });
@@ -113,6 +101,45 @@
         totalPrice = price;
         totalCounter.innerText = '';
         totalCounter.innerText = 'Total: ' + totalPrice + '$';
+    };
+
+    /** Function deselects all the checkboxes*/
+    var resetActivities = function () {
+        [].forEach.call(activities, function (activity) {
+            activity.checked = false;
+        });
+    };
+
+    /**
+     * Function finds the day of the week and scheduled time for an activity
+     * @param {string} text - the full text in a <label> element
+     * @return {string} - a day of the week and scheduled time for an activity
+     */
+    var getActivityTime = function (text) {
+        return String(text.match(/[A-Z,a-z]{1,6}day\s\d{1,2}[apm]{2}/));
+    };
+
+    /** Function finds and disables the activities that conflict with the one that we have chose */
+    var disableConflictingActivities = function (event) {
+        var target = event.target;
+        var activityTime = getActivityTime(target.parentNode.innerText);
+
+        [].forEach.call(activities, function (activity) {
+            var label = activity.parentNode;
+
+            if (activity !== target && label.innerText.indexOf(activityTime) !== -1) {
+                switch (activity.disabled) {
+                    case true:
+                        activity.disabled = false;
+                        label.style.opacity = '1';
+                        break;
+                    case false:
+                        activity.disabled = true;
+                        label.style.opacity = '0.2';
+                        break;
+                }
+            }
+        });
     };
 
     /** Event handler for DOMContentLoaded event */
@@ -143,6 +170,9 @@
 
         /** Initializing activities total price counter */
         countActivitiesTotalPrice();
+
+        /** Deselect all the checkboxes in 'Register for Activities' section */
+        resetActivities();
     };
 
     /** Event handler for 'change' event on the list of job roles */
@@ -220,5 +250,11 @@
     /** Adding an event listener for 'change' event on the list of available activities */
     activitiesList.addEventListener('change', function () {
         countActivitiesTotalPrice();
+
+    });
+
+    /** Adding an event listener for 'change' event on the activities checkboxes */
+    [].forEach.call(activities, function (activity) {
+       activity.addEventListener('change', disableConflictingActivities);
     });
 })();
